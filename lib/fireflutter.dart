d4f96aa2c41003b335bcf30b537fe1ea298a898f
JAEHO SONG <thruthesky@gmail.com>
2020-10-26 22:14:50 +0900

    update for flutterdart packages

--- lib/fireflutter.dart
+++ lib/fireflutter.dart
@@ -1,7 +1,43 @@
 library fireflutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireFlutter {
+  /// Register into Firebase with email/password
+  ///
+  Future<User> register({
+    @required String email,
+    @required String password,
+    String displayName = '',
+    String photoURL = '',
+    Map<String, dynamic> data,
+  }) async {
+    UserCredential userCredential =
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    /// For registraion, it is okay that displayName or photoUrl is empty.
+    await userCredential.user.updateProfile(
+      displayName: displayName,
+      photoURL: photoURL,
+    );
+
+    await userCredential.user.reload();
+    User user = FirebaseAuth.instance.currentUser;
+
+    print('FireFlutter::register() success: user: ');
+    print(user);
+
+    /// Login Success
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    /// Set user extra information
+    await users.doc(userCredential.user.uid).set(data);
+
+    return user;
+  }
 }
