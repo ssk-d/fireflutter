6a60a473e1dc05099673785bc1d69761db5e2e71
JAEHO SONG <thruthesky@gmail.com>
2020-10-29 15:37:44 +0900

    update for kakao login & apple login

--- lib/fireflutter.dart
+++ lib/fireflutter.dart
@@ -12,9 +12,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:rxdart/subjects.dart';
-
+import './functions.dart';
 part './definitions.dart';
-part './functions.dart';
 part './base.dart';
 
 /// FireFlutter
@@ -427,6 +426,15 @@ class FireFlutter extends Base {
 
     onSocialLogin(userCredential.user);
 
+    return userCredential.user;
+  }
+
+  Future<User> signInWithApple() async {
+    final oauthCred = await createAppleOAuthCred();
+    print(oauthCred);
+
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(oauthCred);
     return userCredential.user;
   }
 }
